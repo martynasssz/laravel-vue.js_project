6ae6ca3a0e5629a9fc3b2ca829a08db4443713b3
unknown <j0h@example.com>
2020-04-26 19:55:51 +0300

    vue roouting, first steps

---
+++ resources/js/app.js
@@ -0,0 +1,23 @@
+require("./bootstrap");
+
+import VueRouter from "vue-router";
+import router from "./routes";
+import Index from "./Index";
+
+window.Vue = require("vue");
+// Vue.component(
+//    "example-component",
+//    require("./components/ExampleComponent.vue").default
+// );
+// Vue.component("example-2", require("./components/Example2.vue").default);
+
+Vue.use(VueRouter);
+
+const app = new Vue({
+    el: "#app",
+    router,
+    components: {
+        "index": Index
+    }
+
+});
