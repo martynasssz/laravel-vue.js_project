1efa31df1f7feb1bb8db9179be220b8690a4d16c
unknown <j0h@example.com>
2020-04-30 22:44:21 +0300

    laravel controller

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Bookable;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,14 +18,15 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('bookables', function (Request $request){
-    return Bookable::all();
-});
+// Route::get('bookables', function (Request $request){
+//     return Bookable::all();
+// });
 
-Route::get('bookables/{id}', function (Request $request, $id) {
-    //return Bookable::find($id);
-    return Bookable::findOrFail($id); //try to find something or it will fail. With no found code if this not found in database.
-    
-});
+// Route::get('bookables/{id}', function (Request $request, $id) {
+//     //return Bookable::find($id);
+//     return Bookable::findOrFail($id); //try to find something or it will fail. With no found code if this not found in database.
+// });
 
+Route::get('bookables', 'Api\BookableController@index');
+Route::get('bookables/{id}', 'Api\BookableController@show');
 
