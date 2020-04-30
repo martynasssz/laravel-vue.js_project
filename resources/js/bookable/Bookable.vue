90f3bb91f8e2878f1dc424d92dda277df9fee6db
unknown <j0h@example.com>
2020-04-30 11:51:12 +0300

    Vue router parameters

---
+++ resources/js/bookable/Bookable.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        xxx
+    </div>
+</template>
+
+<script>
+export default {
+    data(){ //get data from server
+       return {  //return an object
+            bookable: null
+       }; 
+    },
+    created() { //create lifcycle hook
+        axios
+          .get(`/api/bookables/${this.$route.params.id}`)
+          .then(response => (this.bookable = response.data));
+    }
+};
+</script>
--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,19 +1,19 @@
 import VueRouter from "vue-router";
-import Example2 from "./components/Example2";
 import Bookables from "./bookables/Bookables";
-
+import Bookable from "./bookable/Bookable";
 
 const routes = [
     {
         path: "/",
         component: Bookables,
         name: "home",
-    },
+    },  
     {
-        path: "/second",
-        component: Example2,
-        name: "second",
+        path: "/bookable/:id",
+        component: Bookable,
+        name: "bookable",
     },
+
 ];
 
 const router = new VueRouter({
