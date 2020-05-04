58d19314368cace7c3dfb49f8d11872ea1ff532b
unknown <j0h@example.com>
2020-05-04 14:52:47 +0300

    Form input bind (v-model)

--- resources/js/bookable/Availability.vue
+++ resources/js/bookable/Availability.vue
@@ -5,18 +5,35 @@
         <div class="form-row">
            <div class="form-group col-md-6">
                <label for="from">From</label>
-               <input type="text" name="from" class="form-control form-control-sm" placeholder="Start date">
+               <input 
+                 type="text" 
+                 name="from" 
+                 class="form-control form-control-sm" 
+                 placeholder="Start date"
+                 v-model="from"
+                 >
            </div>
 
             <div class="form-group col-md-6">
                <label for="to">To</label>
-               <input type="text" name="to" class="form-control form-control-sm" placeholder="End date">
+               <input type="text" name="to" class="form-control form-control-sm" placeholder="End date" v-model="to">
            </div>
         </div>
 
         <button class="btn btn-secondary btn-block">Check!</button>
     </div>
 </template>
+
+<script>
+export default {
+    data() {  //add method data
+        return {   //return javascript object
+            from: null,       //property //intitially it is null
+            to: null          //property //intitially it is null
+        }
+    }
+}
+</script>
 
 <style scoped>
     label {
