0f8a8a35628ac707c69c294758459fd1028a6608
unknown <j0h@example.com>
2020-05-04 14:41:58 +0300

    Availability componnet (horizontal forms, scoped styles)

--- resources/js/bookable/Bookable.vue
+++ resources/js/bookable/Bookable.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="row">
-        <div class="col-md-8">
+        <div class="col-md-8 pb-4">
             <div class="card">
                 <div class="card-body">
                     <div v-if="!loading">
@@ -12,14 +12,18 @@
                 </div>                
             </div>
         </div>
-        <div class="col-md-4">
-            availability & prices
+        <div class="col-md-4 pb-4">
+            <availability></availability>     
         </div>
     </div>
 </template>
 
 <script>
+import Availability from "./Availability";
 export default {
+    components: { //property
+        Availability
+    },
     data(){ //get data from server
        return {  //return an object
             bookable: null,
