7b6a0affd4de64cf616390283674cd8efd9e971b
unknown <j0h@example.com>
2020-05-05 17:07:08 +0300

    Eloquent Local Query Scopes

--- app/Booking.php
+++ app/Booking.php
@@ -2,7 +2,9 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+
 
 class Booking extends Model
 {
@@ -10,5 +12,10 @@ class Booking extends Model
 
     public function bookable() {
         return $this->belongsTo(Bookable::class);
+    }
+
+    public function scopeBetweenDates(Builder $query, $from, $to) {
+        return $query->where('to', '>=' , $from)
+            ->where('from', '<=' , $to);
     }
 }
