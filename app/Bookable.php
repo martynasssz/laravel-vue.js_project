3b9d8624c549c58b6f834f83f12926a9501eb111
unknown <j0h@example.com>
2020-05-05 17:28:39 +0300

    Returning bookable availability (HTTP responce statuses)

--- app/Bookable.php
+++ app/Bookable.php
@@ -9,4 +9,8 @@ class Bookable extends Model
     public function bookings() {
         return $this->hasMany(Booking::class);
     }
+
+    public function availableFor($from, $to): bool {
+        return 0 == $this->bookings()->betweenDates($from, $to)->count();
+    }
 }
