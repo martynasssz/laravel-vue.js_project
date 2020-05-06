79914593705204fdb5de5853ca7e9382f7688145
unknown <j0h@example.com>
2020-05-06 21:53:47 +0300

    ReviewList component markup (using responsive grid options) important to remember some basics

--- resources/js/bookable/Bookable.vue
+++ resources/js/bookable/Bookable.vue
@@ -11,6 +11,8 @@
                     <div v-else>Loading..</div>
                 </div>                
             </div>
+
+            <review-list></review-list>
         </div>
         <div class="col-md-4 pb-4">
             <availability></availability>     
@@ -20,9 +22,11 @@
 
 <script>
 import Availability from "./Availability";
+import ReviewList from "./ReviewList";
 export default {
     components: { //property
-        Availability
+        Availability,
+        ReviewList
     },
     data(){ //get data from server
        return {  //return an object
