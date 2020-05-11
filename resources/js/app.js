6f602ca3729eb12d41bfd3cb80407f97a67df97c
unknown <j0h@example.com>
2020-05-11 12:24:02 +0300

    Star Rating conent (using FontAwesome) IMPORTANT how to create star rating (first part)

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,8 @@ import router from "./routes";
 import Index from "./Index";
 import moment from "moment";
 
+import StarRating from "./shared/components/StarRating";
+
 window.Vue = require("vue");
 // Vue.component(
 //    "example-component",
@@ -15,6 +17,8 @@ window.Vue = require("vue");
 Vue.use(VueRouter);
 
 Vue.filter("fromNow", value => moment(value).fromNow());
+
+Vue.component("star-rating", StarRating); //register global component
 
 
 const app = new Vue({
