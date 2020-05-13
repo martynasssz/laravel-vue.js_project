e720a3582892a11b70091d6855353ae5dd037a8b
unknown <j0h@example.com>
2020-05-13 17:22:27 +0300

    A new API endpoint for storing a review (validation rules, complex flow) TESTED IN POSTMAN HOW TO CREATE A REVIEW FROM EXISTING UUID

--- app/Review.php
+++ app/Review.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
+    protected $fillable = ['id', 'content', 'rating']; //mass asign
+    
     public function bookable()
     {
         return $this->belongsTo(Bookable::class);
