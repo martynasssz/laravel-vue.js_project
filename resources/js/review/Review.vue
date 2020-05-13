39edb947c95e263d16d52a1aea0b30f01c31586a
unknown <j0h@example.com>
2020-05-13 18:53:25 +0300

    Handling request errors in Vue, CREATING HELPER response.js

--- resources/js/review/Review.vue
+++ resources/js/review/Review.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="row">
-    <div :class="[{'col-md-4': loading || !alreadyReviewed}, {'d-none': !loading && alreadyReviewed}]">
+  <div>
+    <div class="row" v-if="error">Unknown error has occured,, please try again later!</div>
+    <div class="row" v-else>
+      <div :class="[{'col-md-4': twoColumns}, {'d-none': oneColumn}]">
       <div class="card">
         <div class="card-body">
           <div v-if="loading">Loading...</div>
-          <div v-else>
+          <div v-if="hasBooking">
             <p>
             Stayed at <router-link :to="{name: 'bookable', params: { id: booking.bookable.bookable_id}}">
               {{ booking.bookable.title }}
@@ -17,7 +19,7 @@
         </div>
       </div>
     </div>
-    <div :class="[{'col-md-8': loading || !alreadyReviewed}, {'col-md-12': !loading && alreadyReviewed}]">
+    <div :class="[{'col-md-8': twoColumns}, {'col-md-12': oneColumn}]">
         <div v-if="loading">Loading...</div>
         <div v-else>
           <div v-if="alreadyReviewed">
@@ -43,11 +45,15 @@
           </div>
         </div>
     </div>
+    </div>
+    
     
   </div>
 </template>
 
 <script>
+import { is404 } from "./../shared/utils/response"; //from response.js file
+
 export default {
     data() {
     return {
@@ -57,7 +63,9 @@ export default {
       },
       existingReview: null,
       loading: false,
-      booking: null
+      booking: null,
+      error: false //general error state
+
       };
     },
     created() { // life cycle hook 
@@ -69,17 +77,22 @@ export default {
         this.existingReview = response.data.data;
         })
         .catch(err => {
-        if (
-          err.response &&
-          err.response.status &&
-          404 === err.response.status
-        ) {
+        if (is404(err)) {
           return axios
             .get(`/api/booking-by-review/${this.$route.params.id}`)
             .then(response => {
               this.booking = response.data.data;
+            })
+            .catch(err => {
+              this.error =!is404(err);    ///----short expresion (cleanway)------////
+              //is404(err) ? {} : (this.error = true);  ///----short expresion------////
+              
+              // if (!is404(err)) {  ///----long expresion------////
+              //     this.error = true;
+              //  }
             });
           }
+          this.error = true;
         })
         .then(() => {
           this.loading = false;
@@ -97,6 +110,12 @@ export default {
       },
       hasBooking() {
         return this.booking !== null;
+      },
+      oneColumn() {
+        return !this.loading && this.alreadyReviewed;
+      },
+      twoColumns() {
+        return this.loading || !this.alreadyReviewed;
       }
     }   
 };
