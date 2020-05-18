04a11646c03c6427b8ea268f1e93a0ec7f9f8d31
unknown <j0h@example.com>
2020-05-18 20:14:02 +0300

    checking what's in the basket, removing from the basket

--- resources/js/bookable/Bookable.vue
+++ resources/js/bookable/Bookable.vue
@@ -1,35 +1,51 @@
 <template>
-    <div class="row">
-        <div class="col-md-8 pb-4">
-            <div class="card">
-                <div class="card-body">
-                    <div v-if="!loading">
-                      <h2>{{ bookable.title }}</h2>
-                      <hr />
-                      <article>{{ bookable.description }}</article>    
-                    </div> 
-                    <div v-else>Loading..</div>
-                </div>                
-            </div>
-
-            <review-list :bookable-id="this.$route.params.id"></review-list>
+  <div class="row">
+    <div class="col-md-8 pb-4">
+      <div class="card">
+        <div class="card-body">
+          <div v-if="!loading">
+            <h2>{{ bookable.title }}</h2>
+            <hr />
+            <article>{{ bookable.description }}</article>
+          </div>
+          <div v-else>Loading...</div>
         </div>
-        <div class="col-md-4 pb-4">
-            <availability 
-              :bookable-id="this.$route.params.id" 
-              @availability="checkPrice($event)"
-              class="mb-4"
-              ></availability>   
+      </div>
 
-              <transition name="fade">
-                <price-breakdown v-if="price" :price="price" class="mb-4"></price-breakdown>
-              </transition>
-              
-              <transition fade>                  
-                <button class="btn btn-outline-secondary btn-block" v-if="price" @click="addToBasket">Book now</button>
-              </transition>                
-        </div>
+      <review-list :bookable-id="this.$route.params.id"></review-list>
     </div>
+    <div class="col-md-4 pb-4">
+      <availability
+        :bookable-id="this.$route.params.id"
+        @availability="checkPrice($event)"
+        class="mb-4"
+      ></availability>
+
+      <transition name="fade">
+        <price-breakdown v-if="price" :price="price" class="mb-4"></price-breakdown>
+      </transition>
+
+      <transition name="fade">
+        <button
+          class="btn btn-outline-secondary btn-block"
+          v-if="price"
+          @click="addToBasket"
+          :disabled="inBasketAlready"
+        >Book now</button>
+      </transition>
+
+      <button
+        class="btn btn-outline-secondary btn-block"
+        v-if="inBasketAlready"
+        @click="removeFromBasket"
+      >Remove from basket</button>
+
+      <div
+        v-if="inBasketAlready"
+        class="mt-4 text-muted warning"
+      >Seems like you've added this object to basket already. If you want to change dates, remove from the basket first.</div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -59,7 +75,18 @@ export default {
         });          
     },
     computed: mapState({
-    lastSearch: "lastSearch"
+    lastSearch: "lastSearch",
+    inBasketAlready(state) {
+        if (null === this.bookable) {
+            return false;
+        }
+
+        return state.basket.items.reduce(
+          (result, item) => result || item.bookable.id === this.bookable.id,
+           false
+        );
+    }
+
   }),
     methods: {
         async checkPrice(hasAvailability) {
@@ -75,13 +102,22 @@ export default {
                 this.price = null;
             }
          },
-            addToBasket() {
+        addToBasket() {
             this.$store.commit("addToBasket", {
                 bookable: this.bookable,
                 price: this.price,
                 dates: this.lastSearch
             });
+        },
+        removeFromBasket() {
+            this.$store.commit("removeFromBasket", this.bookable.id);
         }
     }
 };
 </script>
+
+<style scoped>
+  .warning {
+      font-size: 0.7rem;
+  }
+</style>
