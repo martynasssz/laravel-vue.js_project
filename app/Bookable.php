8425a282ea7492e35aceb7208c28f389fd7a93ba
unknown <j0h@example.com>
2020-06-07 22:13:20 +0300

    Refactoring price calculation

--- app/Bookable.php
+++ app/Bookable.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Illuminate\Support\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Bookable extends Model
@@ -16,7 +17,21 @@ class Bookable extends Model
         return $this->hasMany(Review::class);
     }
 
-    public function availableFor($from, $to): bool {
+    public function availableFor($from, $to): bool 
+    {
         return 0 === $this->bookings()->betweenDates($from, $to)->count();
+    }
+
+    public function priceFor($from, $to): array
+    {
+        $days = (new Carbon($from))->diffInDays(new Carbon($to)) + 1;
+        $price = $days * $this->price;
+
+        return [
+            'total' => $price,
+            'breakdown' => [
+                    $this->price => $days 
+            ]
+        ];    
     }
 }
